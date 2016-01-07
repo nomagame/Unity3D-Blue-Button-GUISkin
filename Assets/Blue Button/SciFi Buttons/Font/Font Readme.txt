bc0383858eb36a993c496aea10b6df2cf1138b3b
Noma <postmaster@nomagame.com>
2016-01-08 01:08:19 +0800

    Blue Button SciFi GUI 2016.1.8
    Blue Button SciFi GUI 2016.1.8

---
+++ Assets/Blue Button/SciFi Buttons/Font/Font Readme.txt
@@ -0,0 +1,2 @@
+"Orion Pax" created by Neale Davidson 
+Free to use
---
+++ Assets/GUIStyle.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GUIStyle : MonoBehaviour {
+
+	public GUISkin myGuiskin;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnGUI(){
+		GUI.skin = myGuiskin;
+		GUI.Button (new Rect (300, 200, 150, 80), "Hello World");
+		GUI.skin = null;
+		GUI.Button (new Rect (500, 200, 150, 80), "New Hello World");
+		GUI.skin = myGuiskin;
+		GUI.Button (new Rect (700, 200, 150, 80), "Hello World");
+		GUI.skin = null;
+		GUI.Button (new Rect (900, 200, 150, 80), "New Hello World");
+	}
+}
